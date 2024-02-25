b20443e1498f9ec2e8d35a365b73c234760576a4
Ethan <44066853+EthanSantos@users.noreply.github.com>
2024-02-25 13:45:30 -0800

    extra components

---
+++ src/components/Information.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Information = () => {
+    return (
+        <div className="infoContainer">
+            <p>Age: Random text</p>
+            <p>Breed: Random text</p>
+            <p>Preferred Activities: Random text</p>
+            <p>About: Random text</p>
+            <p>Ideal Home: Random text</p>
+        </div>
+    )
+}
+
+export default Information;
