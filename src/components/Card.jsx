50f589e3ae6f6125488c4a517192809f7b32406a
Ethan <44066853+EthanSantos@users.noreply.github.com>
2024-02-25 12:45:03 -0800

    removed unnecessary code

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,14 +6,13 @@ import TinderCard from 'react-tinder-card'
 import "./Card.css"
 
 const apiKey = 'live_7WuvcHp9i8PCrYX8TCCjLBTGWBYrBePyokoKu6MsumLbGCxNoo1EsgCggF5fcrBg';
-const apiUrl = 'https://api.thecatapi.com/v1/images/search?limit=50';
+const apiUrl = 'https://api.thecatapi.com/v1/images/search?limit=10';
 
 const headers = {
     'x-api-key': apiKey
 };
 
 const Card = () => {
-    const [lastDirection, setLastDirection] = useState()
     const [cats, setCats] = useState([])
 
     useEffect(() => {
@@ -29,11 +28,6 @@ const Card = () => {
         fetchImages();
     }, []);
 
-    const swiped = (direction, nameToDelete) => {
-        console.log('removing: ' + nameToDelete)
-        setLastDirection(direction)
-    }
-
     const outOfFrame = (name) => {
         console.log(name + ' left the screen!')
     }
@@ -42,14 +36,13 @@ const Card = () => {
         <div>
             <div className='cardContainer'>
                 {cats.map((cat) =>
-                    <TinderCard className='swipe' key={cat.id} onSwipe={(dir) => swiped(dir, cat.id)} onCardLeftScreen={() => outOfFrame(cat.id)}>
+                    <TinderCard className='swipe' key={cat.id} onCardLeftScreen={() => outOfFrame(cat.id)}>
                         <div style={{ backgroundImage: 'url(' + cat.url + ')' }} className='card'>
-                            <h3>{cat.id}</h3>
+                            <h3>Jimmy</h3>
                         </div>
                     </TinderCard>
                 )}
             </div>
-            {lastDirection ? <h2 className='infoText'>You swiped {lastDirection}</h2> : <h2 className='infoText' />}
         </div>
     )
 }
