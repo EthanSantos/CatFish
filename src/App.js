0d06b87634f304f47b1a454a154ac5ae64898dd1
Ethan <44066853+EthanSantos@users.noreply.github.com>
2024-02-25 09:43:15 -0800

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React from "react"
+import Card from "./components/Card"
+
 function App() {
+
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <div className="App">
+      <Card />
     </div>
   );
 }
