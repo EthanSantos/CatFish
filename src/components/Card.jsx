e380db49ec895cab506a4c7a69ac9c2dd077f8fa
Ethan <44066853+EthanSantos@users.noreply.github.com>
2024-02-25 14:40:09 -0800

    latest update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,46 +2,42 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import TinderCard from "react-tinder-card";
 const apiKey =
-  "live_7WuvcHp9i8PCrYX8TCCjLBTGWBYrBePyokoKu6MsumLbGCxNoo1EsgCggF5fcrBg";
+    "live_7WuvcHp9i8PCrYX8TCCjLBTGWBYrBePyokoKu6MsumLbGCxNoo1EsgCggF5fcrBg";
 const apiUrl = "https://api.thecatapi.com/v1/images/search?limit=10";
 
 const headers = {
-  "x-api-key": apiKey,
+    "x-api-key": apiKey,
 };
 
 const Card = () => {
     const [cats, setCats] = useState([])
     const [lastDirection, setLastDirection] = useState()
-  const [cats, setCats] = useState([]);
 
-  useEffect(() => {
-    const fetchImages = async () => {
-      try {
-        const response = await axios.get(apiUrl, { headers });
-        setCats(response.data);
-      } catch (error) {
-        console.error("Error occurred:", error.response.data);
-      }
-    };
+    useEffect(() => {
+        const fetchImages = async () => {
+            try {
+                const response = await axios.get(apiUrl, { headers });
+                setCats(response.data);
+            } catch (error) {
+                console.error("Error occurred:", error.response.data);
+            }
+        };
 
         fetchImages();
     }, []);
 
     const swiped = (direction, catId) => {
         setLastDirection(direction)
-        setCats(cats.filter(cat => cat.id!== catId))
+        setCats(cats.filter(cat => cat.id !== catId))
         console.log(cats.length)
     }
-    
-    fetchImages();
-  }, []);
 
     const outOfFrame = (name) => {
         console.log(name + ' left the screen!')
     }
 
     {
-        if (cats.length === 0){ // no more cats to display
+        if (cats.length === 0) { // no more cats to display
             return (
                 <div>
                     <h1>No more cats to display</h1>
@@ -50,9 +46,6 @@ const Card = () => {
         }
 
     }
-  const outOfFrame = (name) => {
-    console.log(name + " left the screen!");
-  };
 
     return (
         <div>
@@ -68,30 +61,6 @@ const Card = () => {
             </div>
         </div>
     )
-}
-  return (
-    <div>
-      <div className="cardContainer">
-        {cats.map((cat) => (
-          <TinderCard
-            className="swipe"
-            key={cat.id}
-            onCardLeftScreen={() => outOfFrame(cat.id)}
-          >
-            <div
-              style={{ backgroundImage: "url(" + cat.url + ")" }}
-              className="card"
-            >
-              <div className="card-info">
-                <div className="name">Jimmy</div>
-                <div className="title">Persian Cat @ Dancing Cat</div>
-              </div>
-            </div>
-          </TinderCard>
-        ))}
-      </div>
-    </div>
-  );
 };
 
 export default Card;
