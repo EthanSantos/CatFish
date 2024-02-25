fd519de63f269c089554fdb3eeac1f1f72c4b4e8
Ethan <44066853+EthanSantos@users.noreply.github.com>
2024-02-25 10:56:52 -0800

    swipeable cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,75 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
+import axios from "axios"
+
+import TinderCard from 'react-tinder-card'
+
+import "./Card.css"
+
+const db = [
+    {
+        name: 'Cat1',
+        url: ''
+    },
+    {
+        name: 'Cat2',
+        url: ''
+    },
+    {
+        name: 'Cat3',
+        url: ''
+    },
+    {
+        name: 'Cat4',
+        url: ''
+    },
+    {
+        name: 'Cat5',
+        url: ''
+    },
+]
 
 const Card = () => {
+    const characters = db
+    const [lastDirection, setLastDirection] = useState()
+
+    useEffect(() => {
+        let cancel
+        axios.get(`https://api.rescuegroups.org/http/v2.json`, { // decode json
+            cancelToken: new axios.CancelToken(c => cancel = c)
+        }).then(res => {
+            console.log(res.data)
+        }).catch(error => {
+            if (!axios.isCancel(error)) {
+                console.error("Error fetching data: ", error);
+            }
+        });
+
+        return () => cancel()
+
+    }, [])
+
+    const swiped = (direction, nameToDelete) => {
+        console.log('removing: ' + nameToDelete)
+        setLastDirection(direction)
+    }
+
+    const outOfFrame = (name) => {
+        console.log(name + ' left the screen!')
+    }
+
     return (
-        <div>Card</div>
+        <div>
+            <div className='cardContainer'>
+                {characters.map((character) =>
+                    <TinderCard className='swipe' key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
+                        <div style={{ backgroundImage: 'url(' + character.url + ')' }} className='card'>
+                            <h3>{character.name}</h3>
+                        </div>
+                    </TinderCard>
+                )}
+            </div>
+            {lastDirection ? <h2 className='infoText'>You swiped {lastDirection}</h2> : <h2 className='infoText' />} 
+        </div>
     )
 }
 
